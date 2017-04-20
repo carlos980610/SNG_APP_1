9ce1420527a00f16e0dabd5b79de832bc8d8a1d8
diegocorrea33 <diego.correa33@hotmail.es>
2017-04-20 01:29:54 -0500

    D-Carga 1
    Diseño muy basico de "Profile" sin usar gridView. "chat_list" casi sin
    tocar.

--- app/src/main/java/com/example/carlosandrs/sng_app_1/Profile.java
+++ app/src/main/java/com/example/carlosandrs/sng_app_1/Profile.java
@@ -2,12 +2,19 @@ package com.example.carlosandrs.sng_app_1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ProgressBar;
 
 public class Profile extends AppCompatActivity {
+
+    ProgressBar prb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        prb =(ProgressBar)findViewById(R.id.progressBar);
+        prb.setProgress(70);
     }
+
+
 }
