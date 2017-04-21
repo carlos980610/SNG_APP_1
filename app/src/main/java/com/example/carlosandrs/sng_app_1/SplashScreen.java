d5bdc7e05cb9ccc3e06cc7a200c72cda757f1c70
diegocorrea33 <diego.correa33@hotmail.es>
2017-04-21 13:08:01 -0500

    D-Cargas 7

---
+++ app/src/main/java/com/example/carlosandrs/sng_app_1/SplashScreen.java
@@ -0,0 +1,32 @@
+package com.example.carlosandrs.sng_app_1;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        Thread myThread = new Thread(){
+
+            @Override
+            public void run() {
+                try {
+                    sleep(1500);
+                    Intent intent = new Intent(getApplicationContext(), Profile.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        myThread.start();
+    }
+}
